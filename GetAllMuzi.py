d59d2cf9e661da919e66000d20917f7219954503
admin <admin>
2016-07-31 07:01:42 +0300

    v0.2

--- GetAllMuzi.py
+++ GetAllMuzi.py
@@ -1,5 +1,6 @@
 # -*- coding: utf-8 -*-
 import os
+import re
 
 import requests
 
@@ -13,11 +14,15 @@ class MuziGetter(object):
 
     def saveData(self, song):
         song_id = song.get('id', '')
-        song_text = song.get('lyrics', '')
-
+        song_text = re.sub(r'[a-zA-Z]+', '', song.get('lyrics', ''))
         if song_text != '':
             with open(os.path.join(self.output, str(song_id)), 'wb') as out_data:
                 out_data.write(song_text.encode())
+
+    def savePerformers(self, performers):
+        with open(os.path.join(self.output, 'performers.txt'), 'wb') as out_data:
+            out_data.writelines(list(map(lambda x: '{}\r\n'.format(x).encode(), performers.values())))
+            #out_data.writelines(list(map(lambda x: '{}\t{}'.format(str(x[0]), x[1].encode()), performers.items())))
 
 
     def getSongsByStyle(self, style_id):
@@ -30,12 +35,14 @@ class MuziGetter(object):
         return results
 
     def save_all(self):
+        performers = {}
         for style in self.muzi_styles.keys():
             for song in self.getSongsByStyle(style):
+                performers[song.get('performer_id', 0)] = song.get('performer', '')
                 self.saveData(song)
+                self.savePerformers(performers)
 
 
 if __name__ == '__main__':
-    
     m = MuziGetter()
     m.save_all()
--- Start.py
+++ Start.py
@@ -2,29 +2,122 @@
 import settings
 import telebot
 import requests
+import urllib
+import os
+
 bot = telebot.TeleBot(settings.token)
 
-@bot.message_handler(content_types=["text"])
-def repeat_all_messages(message): # Название функции не играет никакой роли, в принципе
-    r = requests.post("http://muzis.ru/api/stream_from_lyrics.api", data={'lyrics': message.text, 'size': 2})
+
+# TODO возвращение музыки в удобном для проигрывания формате - Саша К
+# TODO классификация песен по запросу - Саша З
+# TODO перевод классификации песен на py3 - Саша З
+# TODO получение нормального текста из гепозиции - ?
+# TODO ? обновление классификатора из предпочтений
+# TODO ? возвращение вики инфы об объекте
+# TODO
+#@bot.message_handler(func=lambda message: True, content_types=['text'])
+#def check_answer(message):
+#    print (message.text)
+
+
+@bot.message_handler(content_types=["location", "venue"])
+def repeat_location_messages(message):
+    print(message.location)
+    message.location.longitude = message.location.longitude +0.0001;
+    message.location.latitude = message.location.latitude + 0.0001;
+    bot.send_location(message.chat.id ,  message.location.latitude, message.location.longitude)
+
+@bot.message_handler(content_types=["text" ])
+def repeat_text_messages(message): # Название функции не играет никакой роли, в принципе
+    #bot.send_chat_action(message.chat.id, 'record_audio')
+
+    r = requests.post("http://muzis.ru/api/stream_from_lyrics.api", data={'lyrics': message.text + ':100', 'size': 1})
     print(r.status_code, r.reason)
-    t = r.text
-    print(t)
+
     t2 = r.json()
     songs = t2.get('songs')
     for song in songs:
-        bot.send_message(message.chat.id, song.get('track_name') + '\r\n\r\n')
-        bot.send_message(message.chat.id, song.get('lyrics'))
-        # http://f.muzis.ru/
-        bot.send_message(message.chat.id, song.get('file_mp3'))
-        #audio = open('http://f.muzis.ru/' + song.get('file_mp3'), 'rb')
-        #bot.send_voice(message.chat.id, 'http://f.muzis.ru/' + song.get('file_mp3')) #, reply_markup=markup)
-        #bot.send_audio(message.chat.id, audio) #, reply_markup=markup)
-        #bot.send_voice()
-    #bot.send_message(message.chat.id, message.text + 'dfdf')
+
+        markup = telebot.types.InlineKeyboardMarkup()
+
+        markup.add(telebot.types.InlineKeyboardButton("Next wav",callback_data= 'next'))
+        markup.add(telebot.types.InlineKeyboardButton("Done", callback_data='done'))
+
+        #markup = telebot.types.ReplyKeyboardHide()
+
+        bot.send_message(message.chat.id, song.get('performer') +  song.get('track_name') + '\r\n\r\n')
+        ##bot.send_message(message.chat.id, song.get('lyrics'))
+
+        fileMP3 = 'music/'+ str(message.chat.id) + '.mp3'
+        f = open(fileMP3, 'wb')
+        f.write(urllib.request.urlopen('http://f.muzis.ru/' + song.get('file_mp3')).read())
+        f.close()
+        bot.send_chat_action(message.chat.id, 'upload_audio')
+
+        audio = open(fileMP3, 'rb')
+        print(audio)
+
+        temp = bot.send_audio(message.chat.id, audio, timeout = 1000, title= song.get('performer') +  song.get('track_name'), reply_markup = markup ) # reply_to_message_id=message.message_id) #, reply_markup=markup)
+        audio.close()
+
+@bot.message_handler(content_types=[ "photo" ])
+def repeat_photo_messages(message): # Название функции не играет никакой роли, в принципе
+    temp = bot.send_chat_action(message.chat.id, "upload_photo")
+    photoI = None
+    maxsize= -1
+    for photo  in message.photo:
+        if photo.file_size > maxsize :
+            maxsize = photo.file_size
+            photoI = photo
+
+    file_info = bot.get_file(photoI.file_id)
+    file = requests.get('https://api.telegram.org/file/bot{0}/{1}'.format(settings.token, file_info.file_path))
+    f = open("photo/"  + str(message.chat.id) + ".jpg", 'wb')
+    f.write(file.content)
+    f.close()
+    # TODO API GOOGLE
+
+
+@bot.callback_query_handler(func=lambda call: True)
+def callback_inline(call):
+    # Если сообщение из чата с ботом
+    if call.message:
+        if call.data == "next":
+            markup = telebot.types.InlineKeyboardMarkup()
+
+            markup.add(telebot.types.InlineKeyboardButton("Next wav", callback_data='next'))
+            markup.add(telebot.types.InlineKeyboardButton("Done", callback_data='done'))
+            r = requests.post("http://muzis.ru/api/stream_from_lyrics.api",
+                              data={'lyrics': 'test' + ':100', 'size': 1})
+            print(r.status_code, r.reason)
+            t2 = r.json()
+            songs = t2.get('songs')
+            song = songs[0]
+            fileMP3 = 'music/'+ str(call.message.chat.id) + '.mp3'
+            f = open(fileMP3, 'wb')
+            f.write(urllib.request.urlopen('http://f.muzis.ru/' + song.get('file_mp3')).read())
+            f.close()
+            bot.send_chat_action(call.message.chat.id, 'upload_audio')
+
+            audio = open(fileMP3, 'rb')
+            print(audio)
+
+            #bot.edit_message_text(chat_id=call.message.chat.id, message_id=call.message.message_id, text=call.message.text)
+            temp = bot.send_voice(call.message.chat.id, audio, timeout = 1000, reply_markup = markup ) # reply_to_message_id=message.message_id) #, reply_markup=markup)
+            audio.close()
+        else:
+            print(1)
+            #bot.edit_message_text(chat_id=call.message.chat.id, message_id=call.message.message_id, text=call.message.text)
+            #bot.edit_message_text(chat_id=call.message.chat.id, message_id=call.message.message_id, text="Пыщь", reply_markup=markup)
+    # Если сообщение из инлайн-режима
+    #elif call.inline_message_id:
+    #    if call.data == "test":
+    #        bot.edit_message_text(inline_message_id=call.inline_message_id, text="Бдыщь")
 
 if __name__ == '__main__':
 
-#    print(t2)
-
+    if os.path.exists('photo') == False:
+        os.mkdir('photo')
+    if ~os.path.exists('music') == False:
+        os.mkdirs('music')
     bot.polling(none_stop=True)
